3b17b9b64c423dc57c82a3eea1aba2ef2bfdaedc
Dmytro.Strumenshchykov <dmitro.strumenshikov@outlook.com>
2024-09-09 19:00:12 +0300

    homework 3 changed

--- HW_03/homework_3_3.js
+++ HW_03/homework_3_3.js
@@ -1,6 +1,8 @@
-let year = 1000;
+let year = 20;
 
-if (year % 10 === 1) {
+if (year >=10 && year<=20) {
+    console.log("Вам " + year + " років")
+} else if (year % 10 === 1) {
   console.log("Вам " + year + " рік");
 } else if ((year % 10 === 2) || (year % 10 === 3) || (year % 10 === 4)) {
  console.log("Вам " + year + " роки");
